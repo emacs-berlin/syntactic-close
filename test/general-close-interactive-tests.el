136cc6ae38b3c09dff25d81141eb9d12ec56e672
Andreas Röhler <andreas.roehler@online.de>
2015-05-02 08:40:49 +0200

    test/general-close-interactive-tests.el added

---
+++ test/general-close-interactive-tests.el
@@ -0,0 +1,36 @@
+;;; general-close-interactive-tests.el --- Tests known to work when called interactively only  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015  Andreas Röhler
+
+;; Author: Andreas Röhler <andreas.roehler@online.de>
+;; Keywords: lisp
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Some tests fail in batch mode for unknown reasons. Comments welcome
+
+;;; Code:
+
+
+(ert-deftest gen-close-ruby-block-test ()
+  (gen-test-with-ruby-buffer "$DBH.SELECT_ALL(\"SELECT \* FROM FOO\") DO |ROW|"
+    (general-close)
+    (should (eq (char-before) ?d))
+    (should (looking-at "end"))))
+
+
+(provide 'general-close-interactive-tests)
+;;; general-close-interactive-tests.el ends here
